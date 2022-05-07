ef9089299173b7c0772ab73c15a3e4fca28cc767
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2022-05-07 17:49:06 +0300

    Do not use callback URL

--- paytrail/urls.py
+++ paytrail/urls.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2020 Data King Ltd
+# Copyright (c) 2014-2022 Data King Ltd
 # See LICENSE file for license details
 
 from django.urls import path
@@ -9,5 +9,4 @@ app_name = 'paytrail'
 urlpatterns = (
     path('success/', SuccessView.as_view(), name='success'),
     path('failure/', FailureView.as_view(), name='failure'),
-    path('notify/<token>/', notification, name='notification')
 )
